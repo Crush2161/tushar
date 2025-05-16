43e2502971ccfe925b83fb3276b67e3742e3e90b
Tushar Gadhe <sudarshantushar04@gmail.com>
2025-05-16 19:07:39 +0530

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,5 +1,6 @@
-from .commands import register_command_handlers
-from .callbacks import register_callback_handlers
+# Use absolute imports for better compatibility with Heroku
+from handlers.commands import register_command_handlers
+from handlers.callbacks import register_callback_handlers
 
 def register_handlers(bot):
     """Register all handlers to the bot"""
