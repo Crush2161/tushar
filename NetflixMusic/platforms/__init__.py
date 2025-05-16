088f6c5cf3262741b85aa9b2a728d6f481e34e3d
Crush2161 <s2595229@gmail.com>
2025-05-16 18:28:37 +0530

    Delete NetflixMusic directory

--- NetflixMusic/platforms/__init__.py
+++
@@ -1,6 +0,0 @@
-from .Apple import AppleAPI
-from .Carbon import CarbonAPI
-from .Resso import RessoAPI
-from .Soundcloud import SoundAPI
-from .Telegram import TeleAPI
-from .Youtube import YouTubeAPI
